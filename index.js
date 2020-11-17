7ee33c173c7f11994a742c3700373a4e32fbe8c3
William Damm <wdamm@powerhrg.com>
2020-11-17 18:46:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const app = "I don't do much."
 
 
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
