6abecacc87cf52ad201f05baceb92ea316b4d33b
William Damm <wdamm@powerhrg.com>
2020-11-17 18:47:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
  
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
   }
 }
+
