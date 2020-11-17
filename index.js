d4d32fc582b087f9428989e7b71db458285df0d8
William Damm <wdamm@powerhrg.com>
2020-11-17 18:48:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ function moveDodgerRight() {
   }
 }
 
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
